b176220f2785db92905c3181aad07d8fb6b92e24
lovrovidovic <lovrovidovic6@gmail.com>
2021-11-08 14:12:20 +0100

    created entities

---
+++ services/Models/Recipe.cs
@@ -0,0 +1,23 @@
+﻿using services.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace services.Models
+{
+    public class Recipe : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int TotalCost { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public User UserId { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
---
+++ services/Models/RecipeIngredient.cs
@@ -0,0 +1,23 @@
+﻿using services.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace services.Models
+{
+    public class RecipeIngredient
+    {
+        public int IngredientId { get; set; }
+
+        public Ingredient Ingredient { get; set; }
+
+        public int RecipeId { get; set; }
+
+        public Recipe Recipe { get; set; }
+
+        public float Quantity { get; set; }
+
+        public Unit Unit { get; set; }
+    }
+}
